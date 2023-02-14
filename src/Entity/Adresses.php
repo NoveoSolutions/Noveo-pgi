d0d4b1369c0d4c12b652b2a6d820ffc28db38459
NoveoSolutions <yfallas@noveo-solutions.com>
2023-02-14 10:12:39 +0100

    ajouts des entités de tests

---
+++ src/Entity/Adresses.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdressesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdressesRepository::class)]
+class Adresses
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $numero = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $voierue = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ville = null;
+
+    #[ORM\Column]
+    private ?int $codepostal = null;
+
+    #[ORM\ManyToMany(targetEntity: Clients::class, inversedBy: 'adresses')]
+    private Collection $clientid;
+
+    public function __construct()
+    {
+        $this->clientid = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(?int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getVoierue(): ?string
+    {
+        return $this->voierue;
+    }
+
+    public function setVoierue(string $voierue): self
+    {
+        $this->voierue = $voierue;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCodepostal(): ?int
+    {
+        return $this->codepostal;
+    }
+
+    public function setCodepostal(int $codepostal): self
+    {
+        $this->codepostal = $codepostal;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Clients>
+     */
+    public function getClientid(): Collection
+    {
+        return $this->clientid;
+    }
+
+    public function addClientid(Clients $clientid): self
+    {
+        if (!$this->clientid->contains($clientid)) {
+            $this->clientid->add($clientid);
+        }
+
+        return $this;
+    }
+
+    public function removeClientid(Clients $clientid): self
+    {
+        $this->clientid->removeElement($clientid);
+
+        return $this;
+    }
+}
---
+++ src/Entity/Clients.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientsRepository::class)]
+class Clients
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column]
+    private ?int $telephone = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $commandes = null;
+
+    #[ORM\ManyToMany(targetEntity: Adresses::class, mappedBy: 'clientid')]
+    private Collection $adresses;
+
+    public function __construct()
+    {
+        $this->adresses = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?int
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(int $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getCommandes(): ?string
+    {
+        return $this->commandes;
+    }
+
+    public function setCommandes(?string $commandes): self
+    {
+        $this->commandes = $commandes;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Adresses>
+     */
+    public function getAdresses(): Collection
+    {
+        return $this->adresses;
+    }
+
+    public function addAdress(Adresses $adress): self
+    {
+        if (!$this->adresses->contains($adress)) {
+            $this->adresses->add($adress);
+            $adress->addClientid($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAdress(Adresses $adress): self
+    {
+        if ($this->adresses->removeElement($adress)) {
+            $adress->removeClientid($this);
+        }
+
+        return $this;
+    }
+}
